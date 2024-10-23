99262f4eead3d88dcd9038e6078ff25fb7a69f97
Umar farooq <103427953+umarfarooq1901@users.noreply.github.com>
2024-10-23 07:51:44 +0530

    fixes

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,19 +1,4 @@
-// const nodemailer = require('nodemailer');
 
-
-// let transporter = nodemailer.createTransport({
-//     host: 'smtpout.secureserver.net', // GoDaddy SMTP server
-//     port: 465, // Port for SSL
-//     // secure: true, // Use SSL
-//     auth: {
-//       user: 'services@stylehouse.world', 
-//       pass: 'Ester327811@' 
-//     }
-//   });
-
-
-
-// module.exports = {transporter}
 
 
 const nodemailer = require('nodemailer');
